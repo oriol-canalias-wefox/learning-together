64ba31b526f3d6242b737f3a047769317a8a4dd9
Oriol Canalias <oriol.canalias@wefox.com>
2024-05-23 09:34:34 +0200

    add base producer

--- workshop-rabbitmq/rabbitmq/producer/src/main/kotlin/com/learning/workshop/rabbitmq/controller/PublishController.kt
+++ workshop-rabbitmq/rabbitmq/producer/src/main/kotlin/com/learning/workshop/rabbitmq/controller/PublishController.kt
@@ -1,5 +1,6 @@
 package com.learning.workshop.rabbitmq.controller
 
+import com.learning.workshop.rabbitmq.domain.Person
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,10 +13,17 @@ import org.springframework.web.bind.annotation.RestController
 class PublishController {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @PostMapping("{exchange}/{routingKey}")
-    fun publish(@PathVariable exchange: String,
-                @PathVariable routingKey: String,
-                @RequestBody payload: String) {
-        logger.info("publish, exchange: $exchange, routingKey: $routingKey, payload: $payload")
+    @PostMapping("string/{exchange}/{routingKey}")
+    fun simplePublish(@PathVariable exchange: String,
+                      @PathVariable routingKey: String,
+                      @RequestBody request: String) {
+        logger.info("simplePublish, exchange: $exchange, routingKey: $routingKey, request: $request")
+    }
+
+    @PostMapping("object/{exchange}/{routingKey}")
+    fun objectPublish(@PathVariable exchange: String,
+                      @PathVariable routingKey: String,
+                      @RequestBody request: Person) {
+        logger.info("objectPublish, exchange: $exchange, routingKey: $routingKey, request: $request")
     }
 }
